49f74bfa32bf8ff757e92834f4e51f645d62f368
Nick Rashchuk <raschyk1993@gmail.com>
2019-09-14 12:23:32 +0300

    Updates

---
+++ precache-manifest.d440cf780eb637913154d365e82bacd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d0d5bd3d45c6e93d09b28a70399fcf",
+    "url": "/DevelopsToday/index.html"
+  },
+  {
+    "revision": "2caacd298f893974859e",
+    "url": "/DevelopsToday/static/css/main.f1159ab6.chunk.css"
+  },
+  {
+    "revision": "c572d6ddce57bd2e7d3b",
+    "url": "/DevelopsToday/static/js/2.39c9e39b.chunk.js"
+  },
+  {
+    "revision": "2caacd298f893974859e",
+    "url": "/DevelopsToday/static/js/main.28424be9.chunk.js"
+  },
+  {
+    "revision": "ee5cda354d7864a0058b",
+    "url": "/DevelopsToday/static/js/runtime~main.53924313.js"
+  }
+]);
